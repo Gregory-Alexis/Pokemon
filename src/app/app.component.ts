d4e1184879686743c05acbfe34c5b4b31ae092b4
Westindiess <gregoryalexiss@gmail.com>
2022-09-10 14:55:40 +0200

    remove the logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,7 @@
-import { Component, OnInit } from "@angular/core";
-import { POKEMONS } from "./mock-pokemon-list";
-import { Pokemon } from "./pokemons";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
 })
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon | undefined;
-
-  ngOnInit(): void {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(PokemonId: string) {
-    const pokemon: Pokemon | undefined = this.pokemonList.find(
-      (pokemon) => pokemon.id == +PokemonId
-    );
-    if (pokemon) {
-      console.log(`Vous avez cliquez sur le pokemon ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Vous avez demandez un pokémon qui n'existe pas`);
-      this.pokemonSelected = pokemon;
-    }
-  }
-}
+export class AppComponent {}
