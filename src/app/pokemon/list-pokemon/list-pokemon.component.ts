aeba60c6492cdc990a450558828abbe66476ef53
Westindiess <gregoryalexiss@gmail.com>
2022-09-10 23:03:21 +0200

    move file & folder to pokemon module folder

---
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { POKEMONS } from "../mock-pokemon-list";
+import { Pokemon } from "../pokemons";
+
+@Component({
+  selector: "app-list-pokemon",
+  templateUrl: "./list-pokemon.component.html",
+})
+export class ListPokemonComponent {
+  pokemonList: Pokemon[] = POKEMONS;
+
+  constructor(private router: Router) {}
+
+  goToPokemonDetails(pokemon: Pokemon) {
+    this.router.navigate(["/pokemons", pokemon.id]);
+  }
+}
