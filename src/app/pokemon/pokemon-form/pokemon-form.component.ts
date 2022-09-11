a36a4b7594f5c273082fb84aac30a30d16b52d63
Westindiess <gregoryalexiss@gmail.com>
2022-09-11 17:35:55 +0200

    pokemon form component

---
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { PokemonService } from "src/app/pokemon.service";
+import { Pokemon } from "../pokemons";
+
+@Component({
+  selector: "app-pokemon-form",
+  templateUrl: "./pokemon-form.component.html",
+})
+export class PokemonFormComponent implements OnInit {
+  @Input() pokemon: Pokemon;
+  type: string[];
+
+  constructor(private pokemonService: PokemonService, private router: Router) {}
+
+  ngOnInit() {
+    this.type = this.pokemonService.getPokemonTypeList();
+  }
+
+  hasType(type: string): boolean {
+    return this.pokemon.types.includes(type);
+  }
+
+  selectType($event: Event, type: string) {
+    const isChecked: boolean = ($event.target as HTMLInputElement).checked;
+
+    if (isChecked) {
+      this.pokemon.types.push(type);
+    } else {
+      const index = this.pokemon.types.indexOf(type);
+      this.pokemon.types.splice(index, 1);
+    }
+  }
+
+  onSubmit() {
+    console.log("Submit form !");
+    this.router.navigate(["/pokemon", this.pokemon.id]);
+  }
+}
