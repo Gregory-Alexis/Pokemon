4cf0fe1e2ab3054506b09c3ddca2d3e2c4986380
Westindiess <gregoryalexiss@gmail.com>
2022-09-12 18:48:58 +0200

    import http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -9,7 +10,7 @@ import { PokemonModule } from "./pokemon/pokemon.module";
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent],
-  imports: [BrowserModule, FormsModule, PokemonModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, PokemonModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
