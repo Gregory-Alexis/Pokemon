5dc206ff2d01063d547a42e09b83cdd55cda35ab
Westindiess <gregoryalexiss@gmail.com>
2022-09-13 20:51:58 +0200

    Error & log code refactoring

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, Observable, of, tap } from "rxjs";
-import { POKEMONS } from "./pokemon/mock-pokemon-list";
 import { Pokemon } from "./pokemon/pokemons";
 
 @Injectable()
@@ -10,22 +9,25 @@ export class PokemonService {
 
   getPokemonList(): Observable<Pokemon[]> {
     return this.http.get<Pokemon[]>("api/pokemons").pipe(
-      tap((res) => console.table(res)),
-      catchError((error) => {
-        console.log(error);
-        return of([]);
-      })
+      tap((res) => this.log(res)),
+      catchError((error) => this.handleError(error, []))
     );
   }
 
   getPokemonById(pokemonId: number): Observable<Pokemon | undefined> {
     return this.http.get<Pokemon>(`api/pokemon/${pokemonId}`).pipe(
-      tap((res) => console.table(res)),
-      catchError((error) => {
-        console.log(error);
-        return of(undefined);
-      })
+      tap((res) => this.log(res)),
+      catchError((error) => this.handleError(error, undefined))
     );
+  }
+
+  private log(res: Pokemon[] | Pokemon | undefined) {
+    console.table(res);
+  }
+
+  private handleError(error: Error, errorValue: any) {
+    console.error(error);
+    return of(errorValue);
   }
 
   getPokemonTypeList(): string[] {
