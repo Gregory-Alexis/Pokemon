5464b5663234d5a5ad53f5cf4ae68c83849c77ab
Westindiess <gregoryalexiss@gmail.com>
2022-09-13 23:56:20 +0200

    add pokemon component

--- src/app/pokemon/add-pokemon/add-pokemon.component.ts
+++ src/app/pokemon/add-pokemon/add-pokemon.component.ts
@@ -1,20 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Pokemon } from "../pokemons";
 
 @Component({
-  selector: 'app-add-pokemon',
+  selector: "app-add-pokemon",
   template: `
-    <p>
-      add-pokemon works!
-    </p>
+    <h2 class="center">Ajouter un pokémon</h2>
+    <app-pokemon-form [pokemon]="pokemon"></app-pokemon-form>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class AddPokemonComponent implements OnInit {
+  pokemon: Pokemon;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.pokemon = new Pokemon();
   }
-
 }
