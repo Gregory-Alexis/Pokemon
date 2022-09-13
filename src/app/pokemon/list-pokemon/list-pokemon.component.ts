246480f38db88b37528109f4bcc9d4bd353ed6f4
Westindiess <gregoryalexiss@gmail.com>
2022-09-13 20:54:59 +0200

    get pokemon list refactoring

--- src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -13,7 +13,7 @@ export class ListPokemonComponent {
   constructor(private router: Router, private pokemonService: PokemonService) {}
 
   ngOnInit() {
-    this.pokemonList = this.pokemonService.getPokemonList();
+    this.pokemonService.getPokemonList().subscribe((pokemonList) => (this.pokemonList = pokemonList));
   }
 
   goToPokemonDetails(pokemon: Pokemon) {
