48ff17b9fea6c61284f4c75afccc65334e20ad48
Westindiess <gregoryalexiss@gmail.com>
2022-09-14 23:45:52 +0200

    search component logic

--- src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Observable, Subject } from "rxjs";
+import { Pokemon } from "../pokemons";
 
 @Component({
-  selector: 'app-search-pokemon',
-  templateUrl: './search-pokemon.component.html',
-  styles: [
-  ]
+  selector: "app-search-pokemon",
+  templateUrl: "./search-pokemon.component.html",
 })
 export class SearchPokemonComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  pokemons$: Observable<Pokemon[]>;
+  searchTerms = new Subject<string>();
+
+  search(term: string) {
+    this.searchTerms.next(term);
   }
 
+  goToDetail(pokemon: Pokemon) {
+    const link = ["/pokemon", pokemon.id];
+    this.router.navigate(link);
+  }
 }
