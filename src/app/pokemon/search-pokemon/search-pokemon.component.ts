f78cc196e5731e0d1a465dc8d29bf38f6f3bd037
Westindiess <gregoryalexiss@gmail.com>
2022-09-15 00:04:40 +0200

    add ngOnInit search logic

--- src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { Observable, Subject } from "rxjs";
+import { debounceTime, distinctUntilChanged, mapTo, Observable, Subject, switchMap } from "rxjs";
+import { PokemonService } from "src/app/pokemon.service";
 import { Pokemon } from "../pokemons";
 
 @Component({
@@ -8,9 +9,15 @@ import { Pokemon } from "../pokemons";
   templateUrl: "./search-pokemon.component.html",
 })
 export class SearchPokemonComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private pokemonService: PokemonService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.pokemons$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term) => this.pokemonService.searchPokemon(term))
+    );
+  }
 
   pokemons$: Observable<Pokemon[]>;
   searchTerms = new Subject<string>();
@@ -20,7 +27,7 @@ export class SearchPokemonComponent implements OnInit {
   }
 
   goToDetail(pokemon: Pokemon) {
-    const link = ["/pokemon", pokemon.id];
+    const link = ["/pokemons", pokemon.id];
     this.router.navigate(link);
   }
 }
